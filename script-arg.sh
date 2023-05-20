35706ff3ff12bb677b0e4574a0239350e1772513
manikantadenduluri <saidurgamanikantadsp143@gmail.com>
2023-05-20 23:46:09 +0530

    list of arguments

---
+++ script-arg.sh
@@ -0,0 +1,34 @@
+echo "script arguments list"
+echo
+echo '  $*'"  shows the arguments "
+echo "\$* value = $*"
+echo
+echo '  $@'"  shows also arguments"
+echo "\$@ value = $@"
+echo
+echo '  $0'"  shows the command like this script name"
+echo "\$0 value = $0"
+echo
+echo '  $1'"  shows the first argument"
+echo "\$1 value = $1"
+echo
+echo '  $2'"  shows the second argument"
+echo "\$2 value = $2"
+echo
+echo '  $3'"  shows the third argument"
+echo "\$3 value = $3"
+echo
+echo '  $4'"  shows the forth argument"
+echo "\$4 value = $4"
+echo
+echo '  $5'"  shows the fifth argument"
+echo "\$5 value = $5"
+echo
+echo '  $#'"  shows the count of the arguments"
+echo "\$# value = $#"
+echo
+echo '  $$'"  shows the pid of the script"
+echo "\$$ value = $$"
+echo
+echo '  $?'"  shows the exit value of the script"
+echo "\$? value = $?"
